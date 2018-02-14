9552927da512f74dd6dfceae1fd6a74e578fbe63
Sam Morgan <s_morgan@me.com>
2018-02-14 13:10:09 +0000

    Implement Sign Out
    * Click a button on the Links page to sign out.

--- app.rb
+++ app.rb
@@ -99,5 +99,11 @@ class BookmarkManager < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/')
+  end
+
   run! if app_file == $0
 end
